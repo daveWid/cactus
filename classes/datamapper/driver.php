be412302898a812f37f3313fd8a5189448533b3e
Dave Widmer <dave@davewidmer.net>
2012-01-04 11:19:35 -0500

    Starting the driver interface.

---
+++ classes/datamapper/driver.php
@@ -0,0 +1,13 @@
+<?php
+namespace DataMapper;
+
+/**
+ * The interface for DataMapper drivers.
+ *
+ * @package    DataMapper
+ * @author     Dave Widmer <dave@davewidmer.net>
+ */
+interface Driver
+{
+	
+}
