e2fdc9a6638827e157922941fc355544811dbf18
Dave Widmer <dave@davewidmer.net>
2012-01-10 10:37:56 -0500

    Fixing the autoloader now that it is in the cactus directory, where it should have been to start.

--- classes/cactus/autoloader.php
+++ classes/cactus/autoloader.php
@@ -11,16 +11,19 @@ namespace Cactus;
 class Autoloader
 {
 	/**
+	 * @var  string  The base path for file loading.
+	 */
+	public static $path = null;
+
+	/**
 	 * Built in class autoloading based on the PSR-0 standards.
 	 *
 	 * @see    https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md
 	 *
 	 * @param  string   $class   The name of the class to autoload
-	 * @return boolean           Was the class found and loaded?
 	 */
 	public static function load($class)
 	{
-		$found = false;
 		$class = ltrim($class, "\\");
 
 		if (strtolower(substr($class, 0, 6)) != "cactus")
@@ -28,16 +31,12 @@ class Autoloader
 			return false;
 		}
 
-		$base = dirname(__FILE__) . DIRECTORY_SEPARATOR;
-		$file = $base . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+		$file = static::$path . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 
 		if (is_file($file))
 		{
 			include $file;
-			$found = true;
 		}
-
-		return $found;
 	}
 
 	/**
@@ -45,7 +44,8 @@ class Autoloader
 	 */
 	public static function register()
 	{
-		spl_autoload_register(array("\\Cactus\\Autoloader", "load"));
+		spl_autoload_register(array(__CLASS__, "load"));
+		static::$path = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."..") . DIRECTORY_SEPARATOR;
 	}
 
 	/**
@@ -53,7 +53,7 @@ class Autoloader
 	 */
 	public static function unregister()
 	{
-		spl_autoload_unregister(array("\\Cactus\\Autoloader", "load"));
+		spl_autoload_unregister(array(__CLASS__, "load"));
 	}
 
 }
