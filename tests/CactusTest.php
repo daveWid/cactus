f15cef2452a04a76910e53037c358893b5726176
Dave Widmer <dave@davewidmer.net>
2012-01-11 10:27:17 -0500

    Adding a few more tests.

--- tests/CactusTest.php
+++ tests/CactusTest.php
@@ -27,11 +27,12 @@ class CactusTest extends \Cactus\Tests\DatabaseTest
 		$model = new \Cactus\Tests\ModelUser;
 		$user = $model->get(1);
 
-		$this->assertEquals('Testy', $user->first_name);
+		$this->assertInstanceOf("\\Cactus\\Tests\\User", $user, "Checking for correct entity class.");
+		$this->assertSame('Testy', $user->first_name, "Reading first name correctly.");
 	}
 
 	/**
-	 * See if we can create a new row
+	 * See if we can create a new Entity
 	 */
 	public function testCreate()
 	{
@@ -45,8 +46,90 @@ class CactusTest extends \Cactus\Tests\DatabaseTest
 		$model = new \Cactus\Tests\ModelUser;
 		$user = new \Cactus\Tests\User($data);
 
-		// Check the save
-		$this->assertNotSame(false, $model->save($user)); // False on save error
-		$this->assertInstanceOf("\\Cactus\\Tests\\User", $user);
+		$this->assertNotSame(false, $model->save($user), "Saving correctly, false would be an error");
+	}
+
+	/**
+	 * A test to make sure the modified data is correctly being set
+	 */
+	public function testModified()
+	{
+		$new = array(
+			'first_name' => "George",
+			'last_name' => "Washington"
+		);
+
+		$model = new \Cactus\Tests\ModelUser;
+		$user = $model->get(1);
+
+		$user->set($new);
+
+		$this->assertSame($new, $user->modified(), "Modified data is correct.");
+	}
+
+	/**
+	 * Runs through the read, update, save procedure
+	 */
+	public function testUpdate()
+	{
+		$model = new \Cactus\Tests\ModelUser;
+		$user = $model->get(1);
+
+		$user->first_name = "Abe";
+		$user->last_name = "Lincoln";
+
+		$this->assertSame(1, $model->save($user), "One affected row on update.");
+		$this->assertSame("Abe", $user->first_name, "Confirm data was updated correctly.");
+	}
+
+	/**
+	 * Deletes (and nulls) an Entity
+	 */
+	public function testDelete()
+	{
+		$model = new \Cactus\Tests\ModelUser;
+		$user = $model->get(1);
+
+		$this->assertSame(1, $model->delete($user), "One affected row on delete.");
+		$this->assertNull($user, "Row has been set to null");
+	}
+
+	/**
+	 * Error when tyring to update new entity.
+	 *
+	 * @expectedException \Cactus\Exception
+	 */
+	public function testErrorOnUpdatingNew()
+	{
+		$model = new \Cactus\Tests\ModelUser;
+		$user = $model->get(1);
+
+		$model->create($user);
+	}
+
+	/**
+	 * Error when trying to create an existing entity.
+	 *
+	 * @expectedException \Cactus\Exception
+	 */
+	public function testErrorOnCreateExisting()
+	{
+		$model = new \Cactus\Tests\ModelUser;
+		$user = new \Cactus\Tests\User(array());
+
+		$model->update($user);
+	}
+
+	/**
+	 * Makes sure an error is thrown on the wrong entity type for a mapper.
+	 *
+	 * @expectedException \Cactus\Exception
+	 */
+	public function testErrorWrongEntityType()
+	{
+		$model = new \Cactus\Tests\ModelUser;
+		$role = new \Cactus\Tests\UserRole(array());
+
+		$model->save($role, "Error: wrong entity type.");
 	}
 }
