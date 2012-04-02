077d6dfc19731edd1e63214348bcc9030d30d8c6
Dave Widmer <dave@davewidmer.net>
2012-04-02 14:39:22 -0400

    Moving the Peyote classes into the classes folder for easier PSR-0 autoloading.

--- bootstrap.php
+++ bootstrap.php
@@ -4,17 +4,9 @@
 require_once "classes".DIRECTORY_SEPARATOR."SplClassLoader.php";
 
 // Register the class files
-$segments = array(dirname(__FILE__), "classes");
-$path = implode(DIRECTORY_SEPARATOR, $segments).DIRECTORY_SEPARATOR;
-$loader = new SplClassLoader("Cactus", $path);
+$path = __DIR__.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR;
+$loader = new SplClassLoader(null, $path);
 $loader->register();
 
-// And autoload the vendor folder too
-array_pop($segments);
-$segments[] = "vendor";
-$path = implode(DIRECTORY_SEPARATOR, $segments).DIRECTORY_SEPARATOR;
-
-$vendor = new SplClassLoader(null, $path);
-$vendor->register();
-
-unset($path, $segments);
+// Cleanup
+unset($path);
