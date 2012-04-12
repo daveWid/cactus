8d454fb3cde3e0c5515fc5f158d6ded1d53bb20c
Dave Widmer <dave@davewidmer.net>
2012-04-12 14:18:43 -0400

    Updating the testing bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,19 @@
 <?php
-// Load up the Cactus autoloader
-include realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."..").DIRECTORY_SEPARATOR."bootstrap.php";
+
+// Setup Autoloading
+require_once __DIR__.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR."SplClassLoader.php";
+
+$parts = array(__DIR__, "..", "classes");
+$path = realpath(implode(DIRECTORY_SEPARATOR, $parts)).DIRECTORY_SEPARATOR;
+$loader = new SplClassLoader(null, $path);
+$loader->register();
+
+unset($parts, $path);
 
 // Load up clases needed for the tests
 foreach (glob("tests/classes/*.php") as $filename)
 {
-    include $filename;
+    include_once $filename;
 }
 
 define("DB_NAME", "cactus_test");
