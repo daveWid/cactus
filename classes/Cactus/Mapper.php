bc05a698d3b90c3263566d9263849c7d9593f465
Dave Widmer <dwidmer@bgsu.edu>
2012-07-03 09:46:56 -0400

    Adding some abstracting crud-esque methods to the Mapper.

--- classes/Cactus/Mapper.php
+++ classes/Cactus/Mapper.php
@@ -13,16 +13,16 @@ abstract class Mapper
 	/**
 	 * @var \Cactus\DataSource  The data source used to get the data
 	 */
-	private static $dataSource;
+	private $dataSource;
 
 	/**
 	 * Fetches the current data source
 	 *
 	 * @return \Cactus\DataSource
 	 */
-	public static function getDataSource()
+	public function getDataSource()
 	{
-		return self::$dataSource;
+		return $this->dataSource;
 	}
 
 	/**
@@ -32,7 +32,45 @@ abstract class Mapper
 	 */
 	public function setDataSource(\Cactus\DataSource $source)
 	{
-		self::$dataSource = $source;
+		$this->dataSource = $source;
 	}
+
+	/**
+	 * Attempt to get a row of data by using the passed in key.
+	 *
+	 * @param  mixed $key  The key that is used to search for a row
+	 * @return \Cactus\Entity
+	 */
+	abstract function fetchOne($key);
+
+	/**
+	 * Get all of the records in the data set.
+	 *
+	 * @return \Cactus\ResultSet
+	 */
+	abstract function fetchAll();
+
+	/**
+	 * Gets all of the records that satisfy the search parameters.
+	 *
+	 * @param  array $params   The search parameters
+	 * @return \Cactus\ResultSet
+	 */
+	abstract function find(array $params = array());
+
+	/**
+	 * Saves an entity.
+	 *
+	 * @param \Cactus\Entity $entity  The entity to save
+	 */
+	abstract function save(\Cactus\Entity & $entity);
+
+	/**
+	 * Deletes an entity.
+	 *
+	 * @param  \Cactus\Entity $entity  The entity to delete
+	 * @return boolean                 Was the delete successful?
+	 */
+	abstract function delete(\Cactus\Entity & $entity);
 
 }
