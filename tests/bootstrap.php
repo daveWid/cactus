007fc5e6f3a10f050204d4c5b57391bb1df511f4
Dave Widmer <dwidmer@bgsu.edu>
2012-07-06 11:07:43 -0400

    Moving over to the composer autoloading.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
 // Setup Autoloading
-require_once __DIR__.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR."SplClassLoader.php";
-
-$path = dirname(__DIR__).DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR;
-$loader = new SplClassLoader("Cactus", $path);
-$loader->register();
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
 
 // Load up clases needed for the tests
 foreach (glob("tests/classes/*.php") as $filename)
